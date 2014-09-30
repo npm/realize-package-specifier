c1bca706c9e4fab76dbb3047e7c2da169d217e9e
Rebecca Turner <turner@mikomi.org>
2014-09-30 18:49:29 -0400

    Default where even when its explicitly passed as null

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ var dz = require("dezalgo")
 var npa = require("npm-package-arg")
 
 module.exports = function (spec, where, cb) {
-  if (where instanceof Function) cb = where, where = "."
+  if (where instanceof Function) cb = where, where = null
+  if (where == null) where = "."
   cb = dz(cb)
   try {
     var dep = npa(spec)
