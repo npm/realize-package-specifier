e72bff439c487b32550f92d23d610d06e792dfc6
Rebecca Turner <turner@mikomi.org>
2014-09-30 18:30:59 -0400

    Refactor for simpler result handling and fully realized spec output

--- index.js
+++ index.js
@@ -9,17 +9,21 @@ module.exports = function (spec, where, cb) {
   cb = dz(cb)
   try {
     var dep = npa(spec)
-    var specpath = path.resolve(where, dep.type == "local" ? dep.spec : spec)
-    fs.stat(specpath, function (er, s) {
-      if (er) return cb(null, dep)
-      if (!s.isDirectory()) return dep.spec = specpath, dep.type = "local", cb(null, dep)
-      fs.stat(path.join(specpath, "package.json"), function (er) {
-        if (er) return cb(null, dep)
-        return dep.spec = specpath, dep.type = "directory", cb(null, dep)
-      })
-    })
   }
   catch (e) {
     return cb(e)
+  }
+  var specpath = path.resolve(where, dep.type == "local" ? dep.spec : spec)
+  fs.stat(specpath, function (er, s) {
+    if (er) return finalize()
+    if (!s.isDirectory()) return finalize("local")
+    fs.stat(path.join(specpath, "package.json"), function (er) {
+      finalize(er ? null : "directory")
+    })
+  })
+  function finalize(type) {
+    if (type != null) dep.type = type
+    if (dep.type == "local" || dep.type == "directory") dep.spec = specpath
+    cb(null, dep)
   }
 }
